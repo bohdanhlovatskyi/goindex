ece88ac5ddb78c8776c9758a2682fe8c91b1b59f
bohdanhlovatskyi <bohdan.hlovatskyi@ucu.edu.ua>
2022-04-22 15:34:18 +0300

    first iteration of reader

--- indexation_util/indexation_util.go
+++ indexation_util/indexation_util.go
@@ -1,10 +1,25 @@
 package indexation_util
 
 import (
+	"archive/zip"
+	"bufio"
+	"bytes"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+type RawFileSource struct {
+	Data []byte
+	Path string
+}
+
+type FileSource struct {
+	Data  string
+	Psath string
+}
 
 func TraverseDirs(path string, path_q chan string) {
 	err := filepath.Walk(path,
@@ -26,4 +41,82 @@ func TraverseDirs(path string, path_q chan string) {
 
 	path_q <- ""
 	close(path_q)
+}
+
+func __read_binary(path string) ([]byte, error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	// calculate the bytes size
+	var size int64 = info.Size()
+	bytes := make([]byte, size)
+
+	// read into buffer
+	buffer := bufio.NewReader(file)
+	_, err = buffer.Read(bytes)
+
+	return bytes, nil
+}
+
+func Reader(path_q chan string, data_q chan RawFileSource) {
+
+	for path := range path_q {
+		// poisson pill, this can be done slsighly less ugly
+		// via some struct with context (method that cab be applied to it)
+		// this will also allow to cancel work at any time
+		if path == "" {
+			data_q <- RawFileSource{[]byte(""), ""}
+			break
+		}
+
+		data, err := __read_binary(path)
+		if err != nil {
+			log.Println("could not read file: ", path, err)
+			continue
+		}
+
+		data_q <- RawFileSource{data, path}
+	}
+
+	close(data_q)
+}
+
+func readZipFile(zf *zip.File) ([]byte, error) {
+	f, err := zf.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return ioutil.ReadAll(f)
+}
+
+func ProcessRawSource(raw []byte) (string, error) {
+	b := bytes.NewReader(raw)
+	reader, err := zip.NewReader(b, int64(len(raw)))
+	if err != nil {
+		return "", err
+	}
+
+	// Read all the files from zip archive
+	var sb strings.Builder
+	for _, zipFile := range reader.File {
+		data, err := readZipFile(zipFile)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		sb.WriteString(string(data) + "\n")
+	}
+
+	return sb.String(), nil
 }
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"path/filepath"
 
 	conf_reader "github.com/bohdanhlovatskyi/goindex.git/conf_reader"
 	iutil "github.com/bohdanhlovatskyi/goindex.git/indexation_util"
@@ -13,9 +15,24 @@ func main() {
 	conf := conf_reader.Get_config(CONFIG_PATH)
 
 	path_q := make(chan string, 100)
+	data_q := make(chan iutil.RawFileSource, 100)
 
 	go iutil.TraverseDirs(conf.Indir, path_q)
-	for path := range path_q {
-		fmt.Println(path, len(path))
+	go iutil.Reader(path_q, data_q)
+
+	for elm := range data_q {
+		var data string
+		var err error
+		rdata, path := elm.Data, elm.Path
+		if filepath.Ext(path) == ".zip" {
+			data, err = iutil.ProcessRawSource(rdata)
+			if err != nil {
+				log.Println("oculd not unzip data:", err)
+			}
+		} else {
+			data = string(rdata)
+		}
+
+		fmt.Println(data)
 	}
 }
